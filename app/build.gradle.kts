ae237bfd460dc687bc2b4411aecb92dda9555197
Ruzakj <93422529+Ruzakj@users.noreply.github.com>
2026-09-15 23:29:02 +0700

    Configure lightweight TV app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+
+android {
+    namespace = "com.ric.tiktoktv"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.ric.tiktoktv"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+    buildTypes { release { isMinifyEnabled = true; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro") } }
+}
+
+dependencies { implementation("androidx.core:core-ktx:1.15.0"); implementation("androidx.appcompat:appcompat:1.7.0") }
